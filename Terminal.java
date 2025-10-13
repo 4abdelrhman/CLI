5dc86e60d5a7e5583d0b9ca07d7b27e887de27f4
Abdelrhman <4abdelrhman@gmail.com>
2025-10-13 15:46:16 +0300

    PWD implemented

--- Terminal.java
+++ Terminal.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
+
 class Parser {
     private String commandName;
     private String[] args;
-
-     // This method divides the input into commandName and args
+    // This method divides the input into commandName and args
     // Returns true if parsing succeeds, false otherwise
     public boolean parse (String input){
         if(input == null || input.trim().isEmpty()){
@@ -30,4 +31,40 @@ class Parser {
 
 public class Terminal {
     Parser parser;
-}
+
+
+    public Terminal() {
+        parser = new Parser();
+    }
+
+    public String pwd(){
+        return System.getProperty("user.dir");
+    }
+
+    public static void main(String[] args){
+            Terminal terminal = new Terminal();
+            Scanner sc = new Scanner(System.in);
+
+            while(true){
+                System.out.print("> ");
+
+                String input = sc.nextLine().trim();
+                if(input.equals("exit")){
+                    System.exit(0);
+                }
+
+                terminal.parser.parse(input);
+                String command = terminal.parser.getCommandName();
+                String[] commandArgs = terminal.parser.getArgs();
+
+                switch(command){
+                    case "pwd":
+                        String path = terminal.pwd();
+                        System.out.println(path);
+                        break;
+                    default:
+                        System.out.println(command + " is not a valid command.");
+                }
+            }
+        }
+    }
